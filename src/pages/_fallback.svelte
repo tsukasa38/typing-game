84d646ca4560829d190b554c066638d94e394fbc
tsukasa38 <104.tsukasa@gmail.com>
2021-05-28 07:10:20 +0000

    Implement Routify Router

---
+++ src/pages/_fallback.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import logo from "../assets/svelte.png";
+</script>
+
+<main>
+    <img src={logo} alt="Svelte Logo" />
+    <h1>404 Not Found</h1>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        margin: 0 auto;
+    }
+
+    img {
+        height: 16rem;
+        width: 16rem;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4rem;
+        font-weight: 100;
+        line-height: 1.1;
+        margin: 2rem auto;
+        max-width: 14rem;
+    }
+
+    @media (min-width: 480px) {
+        h1 {
+            max-width: none;
+        }
+    }
+</style>
