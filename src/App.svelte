a42a46ce7c9c7954bbf0777f421bc47798c1020a
tsukasa38 <104.tsukasa@gmail.com>
2021-05-29 08:40:33 +0000

    Add Global CSS

--- src/App.svelte
+++ src/App.svelte
@@ -4,3 +4,15 @@
 </script>
 
 <Router {routes} />
+
+<style>
+  :global(:root) {
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+			Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+	}
+  
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+</style>
