1296b175ffade6024f4a5643ec8d2c4a139a21ab
tsukasa38 <104.tsukasa@gmail.com>
2021-06-07 09:18:38 +0000

    Implement Background-Image

--- src/App.svelte
+++ src/App.svelte
@@ -10,9 +10,15 @@
 		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
 			Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 	}
-  
   :global(body) {
     margin: 0;
     padding: 0;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url("assets/image.svg");
   }
 </style>
