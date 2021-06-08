bd9eb583f621fa0126d9fef716217b91614e8ecd
tsukasa38 <104.tsukasa@gmail.com>
2021-06-08 04:16:12 +0000

    Support PWA

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import App from './App.svelte'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW()
 
 const app = new App({
   target: document.getElementById('app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,52 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import svelte from '@sveltejs/vite-plugin-svelte'
 
-// https://vitejs.dev/config/
+const manifest = {
+  "name": "タイピングゲーム",
+  "short_name": "タイピングゲーム",
+  "lang": "ja",
+  "start_url": "/",
+  "display": "standalone",
+  "orientation": "landscape",
+  "background_color": "#fff",
+  "theme_color": "#c62368",
+  "icons": [
+    {
+      "src": "icons/icon-48.png",
+      "type": "image/png",
+      "sizes": "48x48"
+    },{
+      "src": "icons/icon-64.png",
+      "type": "image/png",
+      "sizes": "64x64"
+    },{
+      "src": "icons/icon-128.png",
+      "type": "image/png",
+      "sizes": "128x128"
+    },{
+      "src": "icons/icon-180.png",
+      "type": "image/png",
+      "sizes": "180x180"
+    },{
+      "src": "icons/icon-192.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },{
+      "src": "icons/icon-256.png",
+      "type": "image/png",
+      "sizes": "256x256"
+    },{
+      "src": "icons/icon-512.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ]
+}
+
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+    svelte(),
+    VitePWA({ manifest })
+  ]
 })
