7695da90d4b0f3c5fa32c18976abc89e4494ffb0
tsukasa38 <104.tsukasa@gmail.com>
2021-06-10 15:43:18 +0000

    Create Timer Component

---
+++ src/components/Timer.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import { onDestroy } from "svelte";
+
+    let frame: number;
+    let active: boolean;
+    let last_time: number;
+    export let power_on: boolean = false;
+    export let remaining_time: number = null;
+
+    $: active = power_on? startTimer(): stopTimer();
+
+    function runningTimer(): void {
+        frame = requestAnimationFrame(runningTimer);
+        const time: number = window.performance.now();
+        remaining_time -= time - last_time;
+
+        if(remaining_time >= 0) { 
+            last_time = time;
+        } else {
+            remaining_time = 0;
+            cancelAnimationFrame(frame);
+        }
+    }
+
+    function startTimer(): boolean {
+        last_time = window.performance.now();
+        runningTimer();
+        return true;
+    }
+
+    function stopTimer(): boolean {
+        cancelAnimationFrame(frame);
+        return false;
+    }
+
+    onDestroy(() => {
+        if(active) {
+            cancelAnimationFrame(frame);
+        }
+    });
+
+</script>
+
+<div class="container">
+    <h1>{(remaining_time/1000).toFixed(2)}</h1>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #ffffff;
+        border-radius: 50%;
+        padding: 2rem 3rem;
+    }
+</style>
